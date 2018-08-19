b31ad6592e56fc05a8a4bc0e5788d85a62d26913
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-08-19 18:34:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // add solution here
 function theBeatlesPlay(musArray, insArray ){
   var myArray = [];
-  for (i=0; )
+  for (i=0; i < musArray; i++){
+    var musician = musArray[i];
+    var instrument = insArray[i];
+    var myString = musician + " plays " + instrument;
+    myArray[i] = myString;
+  }
+  return myArray;
 }
+
+function johnLennonFacts()
